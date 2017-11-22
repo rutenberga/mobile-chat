09ebc7c517058298334b2aac9bc80058d3f54e52
rutenberga <rutarutenberga@gmail.com>
2017-11-22 11:34:11 -0500

    First version Ionic chat complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,9 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
+
+declare var window;
+
 @Component({
   templateUrl: 'app.html'
 })
@@ -16,6 +19,20 @@ export class MyApp {
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+
+      window["ApiAIPlugin"].init(
+        {
+            clientAccessToken: "dd8b60e301a94aac8d12a5d5be6eaaa2", // insert your client access key here 
+            lang: "en" // set lang tag from list of supported languages 
+        }, 
+        function(result) {
+          alert(result)
+        },
+        function(error) { 
+          alert(error)
+         }
+    );
+
     });
   }
 }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,11 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, NgZone, ViewChild } from '@angular/core';
+import { NavController, Content } from 'ionic-angular';
+import { TextToSpeech } from '@ionic-native/text-to-speech';
+// import { ViewChild } from '@angular/core/src/metadata/di';
+// import { Content } from 'ionic-angular/components/content/content';
+// import { Container } from '@angular/compiler/src/i18n/i18n_ast';
+
+declare var window;
 
 @Component({
   selector: 'page-home',
@@ -7,7 +13,60 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  messages: any[] = [];
+  text: string = "";
+  @ViewChild(Content) content: Content;
+
+  constructor(public navCtrl: NavController, public ngZone: NgZone, public tts: TextToSpeech,) {
+
+    this.messages.push({
+      text: "Hi, how can I help you?",
+      sender: "api"
+    })
+
+  }
+
+  sendText(){
+
+    let message = this.text;
+
+    this.messages.push({
+      text: message,
+      sender: 'me'
+    });
+    this.content.scrollToBottom(200);
+
+    this.text = "";
+
+    window["ApiAIPlugin"].requestText({
+      query: message
+    }, (response)=>{
+      this.ngZone.run(()=>{
+        this.messages.push({
+          text: response.result.fulfillment.speech,
+          sender: "api"
+        });
+        this.content.scrollToBottom(200);
+      })
+
+     
+    }, (error)=>{
+      alert(JSON.stringify(error))
+    })
+  }
+
+  sendVoice(){
+    window["ApiAIPlugin"].requestVoice({}, 
+    (response) => {
+      this.tts.speak({
+        text: response.result.fulfillment.speech,
+        locale: "en-US",
+        rate: 1
+      })
+    }, (error) => {
+      alert(error)
+    }
+    )
 
   }
 
